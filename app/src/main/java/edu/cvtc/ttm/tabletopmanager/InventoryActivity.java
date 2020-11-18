2b22b2e7732cde26c58333cfc49dcfe17637da95
Patrick O'Connell <poconnell6@student.cvtc.edu>
2020-11-18 13:29:42 -0600

    Initial commit

---
+++ app/src/main/java/edu/cvtc/ttm/tabletopmanager/InventoryActivity.java
@@ -0,0 +1,120 @@
+package edu.cvtc.ttm.tabletopmanager;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import edu.cvtc.ttm.tabletopmanager.DatabaseContract.InventoryTable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    EditText itemName;
+    EditText itemWeight;
+    EditText itemCost;
+    TextView title;
+    ListView inventoryDisplay;
+    DatabaseHelper dbHelper;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+
+        dbHelper = new DatabaseHelper(this);
+
+        Intent intent = getIntent();
+
+        final String selectedCharName = intent.getExtras().getString("name");
+        //final Integer passedCharID = intent.getExtras().getInt("id");
+        //final String selectedCharID = passedCharID.toString();
+
+        itemName = findViewById(R.id.itemNameEditText);
+        itemWeight = findViewById(R.id.itemWeightEditText);
+        itemCost = findViewById(R.id.itemCostEditText);
+        inventoryDisplay = findViewById(R.id.inventoryList);
+        title = findViewById(R.id.inventoryTitle);
+        title.setText(selectedCharName + "'s Inventory");
+
+        final Button itemEntryButton = findViewById(R.id.itemEntryButton);
+        itemEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                String newItemName = itemName.getText().toString();
+                String newItemWeight = itemWeight.getText().toString();
+                String newItemCost = itemCost.getText().toString();
+
+                dbHelper.addNewItem(selectedCharName, newItemName, newItemWeight, newItemCost, db);
+
+                getCharacterInventory(selectedCharName);
+
+            }
+        });
+
+        // TODO implement this later if I can figure it out
+        /*final Button itemDeleteButton = findViewById(R.id.itemDeleteButton);
+        itemEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                String newItemName = itemName.getText().toString();
+                String newItemWeight = itemWeight.getText().toString();
+                String newItemCost = itemCost.getText().toString();
+
+                dbHelper.addNewItem(selectedCharName, newItemName, newItemWeight, newItemCost, db);
+
+                getCharacterInventory(selectedCharName);
+
+            }
+        });*/
+
+        inventoryDisplay.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView itemView = inventoryDisplay.getChildAt(position).findViewById(R.id.c1);
+                String deleteItemName = itemView.getText().toString();
+
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                dbHelper.deleteItem(deleteItemName, selectedCharName, db);
+                Toast.makeText(view.getContext(), deleteItemName, Toast.LENGTH_LONG).show();
+                getCharacterInventory(selectedCharName);
+
+                return true;
+            }
+        });
+
+        getCharacterInventory(selectedCharName);
+
+    }
+
+    private void getCharacterInventory(String charName) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = dbHelper.fetchInventoryData(db, charName);
+
+        ListAdapter myAdapter = new SimpleCursorAdapter(this, R.layout.inventory_list_diplay,
+                cursor,
+                new String[]{InventoryTable.COLUMN_ITEM_NAME, InventoryTable.COLUMN_ITEM_WEIGHT, InventoryTable.COLUMN_ITEM_COST},
+                new int[]{R.id.cName, R.id.cWeight, R.id.cCost}, 0);
+        inventoryDisplay.setAdapter(myAdapter);
+
+    }
+
+}
---
+++ app/src/main/java/edu/cvtc/ttm/tabletopmanager/MainActivity.java
@@ -0,0 +1,167 @@
+package edu.cvtc.ttm.tabletopmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import edu.cvtc.ttm.tabletopmanager.DatabaseContract.CharactersTable;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText charName;
+    DatabaseHelper dbHelper;
+    ListView charList;
+    Button selectedChar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        charName = findViewById(R.id.characterEditText);
+        dbHelper = new DatabaseHelper(this);
+        charList = findViewById(R.id.characterListView);
+
+        final Button addCharacterButton = findViewById(R.id.addCharacterButton);
+        addCharacterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newCharacterName = charName.getText().toString();
+
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                if (newCharacterName.matches("")) {
+                    Toast.makeText(view.getContext(), "Please enter a character name", Toast.LENGTH_LONG).show();
+                } else {
+                    dbHelper.newCharacter(newCharacterName, db);
+                    getCharacterData();
+                    charName.getText().clear();
+
+                    hideKeyboard(MainActivity.this);
+                }
+
+            }
+        });
+
+        // Some problems to work out here:
+        // Remove character button crashes app if there are too many (5+) characters
+        // Long press deletes character but then clicks character that ends up in its position- not that bad but not good practice either
+        Button removeCharacterButton = findViewById(R.id.removeCharacterButton);
+        removeCharacterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                for(int i = 0; i <= charList.getCount()-1; i++) {
+
+                    selectedChar = charList.getChildAt(i).findViewById(R.id.confirmDeleteButton);
+                    selectedChar.setVisibility(View.VISIBLE);
+
+                    final int position = i;
+
+                    final Button removeCharacterFinal = selectedChar;
+                    removeCharacterFinal.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View view) {
+                            TextView charView = charList.getChildAt(position).findViewById(R.id.idnum);
+                            String deleteCharacterName = charView.getText().toString();
+
+                            SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                            dbHelper.deleteCharacter(deleteCharacterName, db);
+                            Toast.makeText(view.getContext(), deleteCharacterName + " has been deleted.", Toast.LENGTH_LONG).show();
+                            getCharacterData();
+
+                            return true;
+                        }
+                    });
+                }
+            }
+
+        });
+
+        charList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SQLiteDatabase db = dbHelper.getReadableDatabase();
+                ArrayList<String> tester = dbHelper.getSelectedCharacter(db);
+
+                String test = tester.get(position);
+
+                Intent intent = new Intent(MainActivity.this, InventoryActivity.class);
+                intent.putExtra("name", test);
+                intent.putExtra("id", position);
+                startActivity(intent);
+            }
+        });
+
+        getCharacterData();
+
+    }
+    // The fact that I had to build a function for this is ...unfortunate...
+    // the fact that this is the best anyone in 2020 can do for this is even worse.
+    // https://stackoverflow.com/questions/1109022/how-do-you-close-hide-the-android-soft-keyboard-using-java
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+        //return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getCharacterData() {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = dbHelper.fetchCharacterData(db);
+
+        ListAdapter myAdapter = new SimpleCursorAdapter(this, R.layout.character_list_display,
+                cursor,
+                new String[]{CharactersTable.COLUMN_CHARACTER_NAME},
+                new int[]{R.id.idnum}, 0);
+        charList.setAdapter(myAdapter);
+
+    }
+}
