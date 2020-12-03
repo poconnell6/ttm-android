4408a8b1654fb46bf1f71012404492841f437a71
Patrick O'Connell <poconnell6@student.cvtc.edu>
2020-12-02 23:19:59 -0600

    Version up DB to prevent explosions.

--- app/src/main/java/edu/cvtc/ttm/tabletopmanager/DatabaseContract.java
+++ app/src/main/java/edu/cvtc/ttm/tabletopmanager/DatabaseContract.java
@@ -5,7 +5,7 @@ import android.provider.BaseColumns;
 public class DatabaseContract {
 
     public static final String DB_NAME = "ttm.com.cvtc.edu.tabletopmanager";
-    public static final int DB_VERSION = 1;
+    public static final int DB_VERSION = 2;
     public static final String _ID = "id";
 
     public class CharactersTable implements BaseColumns {
